c8969f5b72d828387a69c34b42a91665add2ba3b
Kentleigh English <kentleigh@ikengainnovations.com>
2022-04-20 14:25:41 +0100

    Add logging to request module

--- request/__init__.py
+++ request/__init__.py
@@ -1,13 +1,15 @@
 import logging;
 import requests;
 
+logger = logging.getLogger("request");
+
 class Request():
 
 	def get(url, data, headers):
-		logging.info("GET request to " + url);
+		logger.info("GET request to " + url);
 		return self.rawRequest(method="get", url=url, data=data, headers=headers);
 	def post(url, data, headers):
-		logging.info("Post Request to " + url);
+		logger.info("POST Request to " + url);
 		return self.rawRequest(method="post", url=url, data=data, headers=headers);
 	def _rawRequest(method, url, data, headers):
 		requestData = {};
