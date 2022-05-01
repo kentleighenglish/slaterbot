85f5b2a90dbf23b3932e010fb187019cc7c82fe9
Kentleigh English <kentleigh@ikengainnovations.com>
2022-05-01 22:33:41 +0100

    Lint all python files

--- request/__init__.py
+++ request/__init__.py
@@ -1,25 +1,51 @@
-import logging;
-import requests;
+"""
+This script holds the class for generating various HTTP requests.
 
-logger = logging.getLogger("request");
+
+"""
+import logging
+import requests
+
+logger = logging.getLogger("request")
+
 
 class Request():
+    """
+    This is a generic Request class for handling HTTP requests.
 
-	def get(url, data, headers):
-		logger.info("GET request to " + url);
-		return self.rawRequest(method="get", url=url, data=data, headers=headers);
-	def post(url, data, headers):
-		logger.info("POST Request to " + url);
-		return self.rawRequest(method="post", url=url, data=data, headers=headers);
-	def _rawRequest(method, url, data, headers):
-		requestData = {};
-		requestHeaders = {};
-		
-		if (data):
-			requestData = data;
-		if (headers):
-			requestHeaders = headers;
-	
-		r = requests[method](url, data=requestData, headers=requestHeaders);
-	
-		return r.json();
+    Contained methods can be used to make GET and POST requests
+    """
+
+    def get(self, url, data, headers):
+        """Run a generic GET request with given data and headers."""
+        logger.info("GET request to " + url)
+        return self.rawRequest(
+            method="get",
+            url=url,
+            data=data,
+            headers=headers
+        )
+
+    def post(self, url, data, headers):
+        """Run a generic POST request with given data and headers."""
+        logger.info("POST Request to " + url)
+        return self.rawRequest(
+            method="post",
+            url=url,
+            data=data,
+            headers=headers
+        )
+
+    def _rawRequest(method, url, data, headers):
+        """Run a generic request with given data and headers."""
+        requestData = {}
+        requestHeaders = {}
+
+        if (data):
+            requestData = data
+        if (headers):
+            requestHeaders = headers
+
+        r = requests[method](url, data=requestData, headers=requestHeaders)
+
+        return r.json()
