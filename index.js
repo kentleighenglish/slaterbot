0ae434050cb01142d9b144c2687bc67a4ba5ea8d
Kentleigh English <kentleigh@ikengainnovations.com>
2023-10-02 13:39:34 +0100

    Get debug working

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
-require("dotenv");
-const Alpaca = require('@alpacahq/alpaca-trade-api');
+import debugFunc from "debug"
+const log = debugFunc("bot:main");
 
-const positions = ["TSLA"];
+import Alpaca from "@alpacahq/alpaca-trade-api";
+
+const symbols = (process.env.SYMBOLS||"").split(",").map(s => s.trim());
+const positions = [];
+let account = null;
+let balance = null;
 
 const alpaca = new Alpaca({
-  keyId: process.env.ALPACA_KEY,
-  secretKey: process.env.ALPACA_SECRET,
-  paper: true,
+	keyId: process.env.ALPACA_KEY,
+	secretKey: process.env.ALPACA_SECRET,
+	paper: true,
 });
 
-const socket = alpaca.data_stream_v2;
+const watchSymbol = async (symbol) => {
+	log(`Watching symbol: ${symbol}`);
+}
 
-socket.onConnect(function () {
-  console.log("Connected");
-  socket.subscribeForQuotes(["AAPL"]);
-  socket.subscribeForTrades(["FB"]);
-  socket.subscribeForBars(["SPY"]);
-  socket.subscribeForStatuses(["*"]);
-});
+const init = async () => {
+	log("Starting bot");
 
-socket.onError((err) => {
-  console.log(err);
-});
+	account = await alpaca.getAccount();
+	balance = account.equity;
 
-socket.onStockTrade((trade) => {
-  console.log(trade);
-});
+	for (let symbol of symbols) {
+		watchSymbol(symbol);
+	}
+	log("Bot initialised");
+}
 
-socket.onStockQuote((quote) => {
-  console.log(quote);
-});
+init();
 
-socket.onStockBar((bar) => {
-  console.log(bar);
-});
+// const socket = alpaca.data_stream_v2;
 
-socket.onStatuses((s) => {
-  console.log(s);
-});
+// socket.onConnect(function () {
+//   console.log("Connected");
+//   socket.subscribeForQuotes(["AAPL"]);
+//   socket.subscribeForTrades(["FB"]);
+//   socket.subscribeForBars(["SPY"]);
+//   socket.subscribeForStatuses(["*"]);
+// });
 
-socket.onStateChange((state) => {
-  console.log(state);
-});
+// socket.onError((err) => {
+//   console.log(err);
+// });
 
-socket.onDisconnect(() => {
-  console.log("Disconnected");
-});
+// socket.onStockTrade((trade) => {
+//   console.log(trade);
+// });
 
-socket.connect();
+// socket.onStockQuote((quote) => {
+//   console.log(quote);
+// });
+
+// socket.onStockBar((bar) => {
+//   console.log(bar);
+// });
+
+// socket.onStatuses((s) => {
+//   console.log(s);
+// });
+
+// socket.onStateChange((state) => {
+//   console.log(state);
+// });
+
+// socket.onDisconnect(() => {
+//   console.log("Disconnected");
+// });
+
+// socket.connect();
