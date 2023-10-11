e180695027c4492af7f4268758129395cd5b1a24
Kentleigh English <kentleigh@ikengainnovations.com>
2023-10-11 09:22:29 +0100

    Committing glitch.com changes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import debugFunc from "debug"
-const log = debugFunc("bot:main");
+const log = require("debug")("bot:main");
 
-import Alpaca from "@alpacahq/alpaca-trade-api";
+const Alpaca = require("@alpacahq/alpaca-trade-api");
 
 const symbols = (process.env.SYMBOLS||"").split(",").map(s => s.trim());
 const positions = [];
 let account = null;
 let balance = null;
+
+let watchlist = null;
+
+const watchers = {};
+
+const CHECK_INTERVAL = 5000;
 
 const alpaca = new Alpaca({
 	keyId: process.env.ALPACA_KEY,
@@ -14,12 +19,53 @@ const alpaca = new Alpaca({
 	paper: true,
 });
 
+const createWatchlist = async () => {
+  try {
+   const watchlists = await alpaca.getWatchlists();
+    
+    watchlists.forEach(w => {
+      if (w.name === "slaterbot") {
+        watchlist = w;
+      }
+    });
+    
+    if (!watchlist) {
+      const response = await alpaca.addWatchlist("slaterbot", []);
+      watchlist = response;
+    }
+  } catch(e) {
+    console.error(e);
+    
+    return;
+  }
+}
+
+const checkSymbol = async (symbol) => {
+  log (`Checking data for ${symbol}`);
+  let closePosition = false;
+  const response = await alpaca.getLatestTrade(symbol);
+  
+  log(response);
+  
+  if (!closePosition) {
+    setTimeout(() => checkSymbol(symbol), CHECK_INTERVAL);
+  }
+}
+
 const watchSymbol = async (symbol) => {
 	log(`Watching symbol: ${symbol}`);
+  
+  log (`Adding ${symbol} to watchlist`);
+  //await alpaca.addToWatchlist(watchlist.id, symbol);
+  
+  // initialise loop
+  checkSymbol(symbol);
 }
 
 const init = async () => {
 	log("Starting bot");
+  
+  await createWatchlist();
 
 	account = await alpaca.getAccount();
 	balance = account.equity;
@@ -31,43 +77,3 @@ const init = async () => {
 }
 
 init();
-
-// const socket = alpaca.data_stream_v2;
-
-// socket.onConnect(function () {
-//   console.log("Connected");
-//   socket.subscribeForQuotes(["AAPL"]);
-//   socket.subscribeForTrades(["FB"]);
-//   socket.subscribeForBars(["SPY"]);
-//   socket.subscribeForStatuses(["*"]);
-// });
-
-// socket.onError((err) => {
-//   console.log(err);
-// });
-
-// socket.onStockTrade((trade) => {
-//   console.log(trade);
-// });
-
-// socket.onStockQuote((quote) => {
-//   console.log(quote);
-// });
-
-// socket.onStockBar((bar) => {
-//   console.log(bar);
-// });
-
-// socket.onStatuses((s) => {
-//   console.log(s);
-// });
-
-// socket.onStateChange((state) => {
-//   console.log(state);
-// });
-
-// socket.onDisconnect(() => {
-//   console.log("Disconnected");
-// });
-
-// socket.connect();
